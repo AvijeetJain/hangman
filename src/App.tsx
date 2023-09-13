7cf69280a5de3cadb1700b401cecac9f2904c97a
AvijeetJain <jainavijeet@gmail.com>
2023-09-13 14:20:10 +0530

    wordsUpdated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import words from './wordList.json'
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("test")
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+
+  console.log(wordToGuess)
   return (
     <div>
       hey i am avijeet
